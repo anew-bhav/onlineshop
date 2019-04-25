6edc71a867103c5da0335fcd344799cad3594192
anubhav8421 <anubhavj1357@gmail.com>
2019-04-25 19:23:21 +0530

    Add seed for Product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Product.delete_all
+
+############### Create Seed for Product #################
+
+IMAGE_URL   = "https://dummyimage.com/300x300/c6c8ee/a30b37&text=Test+Product"
+
+20.times do
+  Product.create!(
+    title: Faker::Commerce.unique.product_name,
+    description: %(<p>#{Faker::Lorem.unique.paragraph(3, true, 6)}</p>),
+    image_url: IMAGE_URL,
+    price: Faker::Commerce.unique.price)
+end
+#########################################################
+
