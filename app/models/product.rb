4b06a95c3b890af4d68f6bd717122f1af19a258e
anubhav8421 <anubhavj1357@gmail.com>
2019-04-26 14:49:29 +0530

    Add validation for Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|jpeg|png)\Z}i, # WHATIF :  URL doesnot link to picture but links to picture resource
+    message: 'must be a URL for image'
+  }
 end
