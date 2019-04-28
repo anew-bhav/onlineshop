854076c494c1493b422f205f151503e544820fc7
anubhav8421 <anubhavj1357@gmail.com>
2019-04-28 17:10:51 +0530

    Enhance test for products_controller index action

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,6 +16,22 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select "tr", class: { in: ["list_line_odd", "list_line_even"] } do
+      assert_select "td" do
+        assert_select "img", class: "list_image"
+      end
+      assert_select "td", class: "list_description" do
+        assert_select "dl" do
+          assert_select "dt"
+          assert_select "dd"
+        end
+      end
+      assert_select "td", class: ".list_actions" do
+        assert_select "a", href: /products\/[0-9]+/, text: "Show"
+        assert_select "a", href: /products\/[0-9]+\/edit/, text: "Edit"
+        assert_select "a", href: /products\/[0-9]+/, text: "Destroy", method: "delete", confirm: "Are you sure?"
+      end
+    end
   end
 
   test "should get new" do
